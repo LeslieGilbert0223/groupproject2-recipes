8548379227769339433b2feff33483fded2ce59a
lesliegilbert0223 <lesliegilbert0223@gmail.com>
2020-11-13 17:10:52 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { HeaderComponent } from './header/header.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchFormComponent,
+    HeaderComponent,
+    FavoritesComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
